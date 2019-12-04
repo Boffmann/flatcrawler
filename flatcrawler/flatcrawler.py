a47807af7367066965ead23a71ed9ecbbd239cf0
Hendrik Tjabben <hendrik.tjabben@student.hpi.de>
2019-12-04 22:07:43 +0100

    Add hash to wg results and store all in internal list

--- flatcrawler/flatcrawler.py
+++ flatcrawler/flatcrawler.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-from python_json_config import Config, ConfigBuilder
+from python_json_config import ConfigBuilder
+from typing import Dict
 import requests as req
+import sys
 
 from .wg_result import WGResult
 from .telegram_sender import SenderTelegram
@@ -14,15 +16,25 @@ class FlatCrawler(object):
         config = builder.parse_config(str(config_path))
 
         self.urls = config.wggesucht.urls
-        self.wg_results = []
         self.telegram_sender = SenderTelegram(config.telegram.bot_token, config.telegram.receiver_ids)
+        # Stores all found WGResults. Everytime the query runs its results are added to this list.
+        # The dictionary only stores unique values to prevent that inserts are sent more than once to the user
+        # self.advert_hash_list = Dict[str, WGResult]
+        self.advert_hash_list: Dict[str, WGResult] = {}
 
     def run(self):
         """Query, parse and present wggesucht results"""
         for url in self.urls:
             wg_html_response = req.get(url)
             if wg_html_response.status_code == 200:
-                wg_results = WGResult.from_html_response(wg_html_response.content)
+                try:
+                    wg_results = WGResult.from_html_response(wg_html_response.content)
+                except:
+                    e = sys.exc_info()[0]
+                    print("Error creating wg_result " + str(e))
+                    continue
                 for result in wg_results:
-                    print(result.as_string())
-                    # self.telegram_sender.sendWGMessage(result)
+                    # To prevent that single adverts are sent twice
+                    if not result.hash in self.advert_hash_list.keys():
+                        self.advert_hash_list[result.hash] = result
+                        print(result.as_string())
--- flatcrawler/wg_result.py
+++ flatcrawler/wg_result.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from bs4.element import Tag
 import re
 import requests as req
+import hashlib
 from types import LambdaType
 
 def get_element_save(element_function: LambdaType, error_msg: str):
@@ -23,17 +24,14 @@ class WGResult(object):
         self.address = None
         self.availability_from = None
         self.availability_to = None
+        self.hash = None
         if len(headline) > 0:
             self.url = self.baseurl + headline[0]['href']
         if self.url is not None:
             # Crawl side of room
             room_html_response = req.get(self.url)
             if room_html_response.status_code == 200:
-                try:
-                    soup = BeautifulSoup(room_html_response.content, 'html.parser')
-                except:
-                    print("Error parsing the website: " + self.url)
-                    return
+                soup = BeautifulSoup(room_html_response.content, 'html.parser')
                 self.title = get_element_save(lambda: soup.find('h1', {'id': 'sliderTopTitle'}).text.strip(), "Error parsing adverts title")
                 main_column = soup.find('div', {'id': 'main_column'})
                 self.size_of_room = get_element_save(lambda: list(main_column.find_all('div', {'class': "col-xs-6 text-center print_inline"}))[0].select("h2")[0].text.strip(), "Error parsing advert's size of room")
@@ -41,7 +39,8 @@ class WGResult(object):
                 self.address = get_element_save(lambda: list(main_column.find_all('div', {'class': "col-sm-4 mb10"}))[0].select("a")[0].text.strip(), "Error parsing advert's address")
                 self.availability_from = get_element_save(lambda: list(main_column.find_all('div', {'class': "col-sm-3"}))[0].select("p")[0].select("b")[0].text.strip(), "Error parsing advert's availability_from")
                 self.availability_to = get_element_save(lambda: list(main_column.find_all('div', {'class': "col-sm-3"}))[0].select("p")[0].select("b")[1].text.strip(), "Error parsing advert's availability_to")
-
+                characteristic_string = str.encode(self.title + self.room_cost + self.size_of_room)
+                self.hash = hashlib.sha256(characteristic_string).hexdigest()[:10]
 
     def as_string(self):
         result = "Title: " + self.title + "\n" \
@@ -50,6 +49,7 @@ class WGResult(object):
                   + "Available from: " + self.availability_from + "\n" \
                   + "Available to: " + self.availability_to + "\n" \
                   + "Address: " + self.address + "\n" \
+                  + "Hash: " + self.hash + "\n" \
                   + "URL: " + self.url
         return result
 
